5282c3297aefe8a6d1edd7adbd0c74c057f0924c
sn42 <dev.sn42@gmail.com>
2018-12-19 15:35:20 +0100

    Change marker type to interface

--- application/src/main/java/com/github/sn42/sample/application/ApplicationMarker.java
+++ application/src/main/java/com/github/sn42/sample/application/ApplicationMarker.java
@@ -1,4 +1,10 @@
 package com.github.sn42.sample.application;
 
-public @interface ApplicationMarker {
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * A no-op marker interface to be referenced by
+ * {@link SpringBootApplication#scanBasePackageClasses()}.
+ */
+public interface ApplicationMarker {
 }

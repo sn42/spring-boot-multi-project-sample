808ee7fb653417075123b2c872b918e0c8cae166
sn42 <dev.sn42@gmail.com>
2018-12-19 14:04:58 +0100

    Remove lombok

--- application/src/main/java/com/github/sn42/sample/application/quote/Quote.java
+++ application/src/main/java/com/github/sn42/sample/application/quote/Quote.java
@@ -1,8 +1,36 @@
 package com.github.sn42.sample.application.quote;
 
-import lombok.Data;
+import java.util.Objects;
 
-@Data
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Quote {
     private final String content;
+
+    @JsonCreator
+    public Quote(@JsonProperty("content") String content) {
+        this.content = Objects.requireNonNull(content);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Quote other = (Quote) obj;
+        return Objects.equals(this.content, other.content);
+    }
 }
